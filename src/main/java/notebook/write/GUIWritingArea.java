cb893402095d8a15ab1f2dda46031e6a6a267e91
Dan Orel <mail.ordan@gmail.com>
2019-06-02 20:59:27 +0300

    Modified the writing area module

--- src/main/java/notebook/write/GUIWritingArea.java
+++ src/main/java/notebook/write/GUIWritingArea.java
@@ -10,13 +10,7 @@ import javax.swing.text.StyledDocument;
 import java.awt.*;
 
 public class GUIWritingArea extends JTextPane {
-    /*
-        Content of the file. It consists of:
-            - title
-            - page content
-     */
-    private StringBuilder html;
-    private StringBuilder body;
+
     /*
             Content of the file is saved in the object of Document class
     */
@@ -27,20 +21,6 @@ public class GUIWritingArea extends JTextPane {
     private SimpleAttributeSet attributes;
 
     public GUIWritingArea(){
-        html = new StringBuilder(
-                "<html>"+
-                    "<head>"+
-                        "<title>"+
-                        "</title>"+
-                    "</head>"+
-                    "<body>"+
-                        "<h3 id=\"title\">"+
-                        "</h3>"+
-                        "<p id=\"text\">"+
-                        "</p>"+
-                    "</body>"+
-                "</html>"
-        );
         document = this.getStyledDocument();
         attributes = new SimpleAttributeSet();
     }
